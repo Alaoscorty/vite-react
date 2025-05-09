1b447aa5206077795f55c929ded9bf68ac205dcd
Alaoscorty <133812869+Alaoscorty@users.noreply.github.com>
2025-05-09 04:24:56 +0100

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'; // 👈 Obligatoire pour que le JSX fonctionne dans certaines configs
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { AuthProvider } from './contexts/AuthContexts';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
