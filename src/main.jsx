f71b7b08d4887190deb748fe7c21b9b25460f752
Alaoscorty <133812869+Alaoscorty@users.noreply.github.com>
2025-05-09 03:22:58 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import App from './App.tsx';
 import { AuthProvider } from './contexts/AuthContexts'; // ← Ajout de l'import
 
 createRoot(document.getElementById('root')).render(
